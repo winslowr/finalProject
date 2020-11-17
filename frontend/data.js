4c6b6492b136d2b52eb5096ac021372f699a203e
treytanjuatco <42626067+treytanjuatco@users.noreply.github.com>
2020-11-16 23:09:02 -0500

    Update data.js

--- frontend/data.js
+++ frontend/data.js
@@ -1,15 +1,3 @@
-async function fetchMoviesJSON() {
-	const response = await fetch("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-key": "1119f077a7msh6ed3381feb84700p1e65c9jsn0a49cf7dca25",
-			"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com"
-		}
-	})
-	const obj = await response.json();
-	return obj;
-  }
-
 /*
 Manipulate object as desired
 	Object has two fields: 
@@ -28,11 +16,73 @@ Manipulate object as desired
 			  j. 'title'
 			  k. 'type': movie or tv show
 			  l. 'unogsdate
-*/	
+*/
+
+
+let movieObjects = []; // Global array of movie objects
+let movieTitles = [];
+let movieImages = [];
+let movieDescription = [];
+
+const myList = document.querySelector('ul');
+const start = document.getElementById('start');
+
+function cardCreator(movie) {
+	return `
+	<div class="column is-one-quarter">
+		<div class="box">
+			<div class="card">
+				<div class="card-image">
+					<figure>
+						<img src="${movie.image}" alt="Placeholder image">
+					</figure>
+				</div>
+				
+				<div class="card-content">
+					<div class="media">
+						<div class="media-content">
+							<p class="title is-4">${movie.title}</p>
+						</div>
+					</div>
+
+					<div class="content">
+						<p> ${movie.synopsis}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	`
+}
+
+async function fetchMoviesJSON() {
+	const response = await fetch("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "1119f077a7msh6ed3381feb84700p1e65c9jsn0a49cf7dca25",
+			"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com"
+		}
+	})
+	const obj = await response.json();
+	return obj;
+}
+
 fetchMoviesJSON().then(obj => {
-	// Array of items
-	let items = obj.ITEMS; 
+	let items = obj.ITEMS;
 	items.forEach(movie => {
-		console.log(movie.title + ': ' + movie.synopsis)
+		/*
+		movieObjects.push(movie);
+		movieTitles.push(movie.title);
+		movieImages.push(movie.image);
+		movieDescription.push(movie.synopsis);
+		*/
+
+		let listyItem = `<p> ${movie.title}</p>`
+		let item = cardCreator(movie)
+		$('#movies').append(item);
 	});
 });
+
+$(function () {
+	//$(document).on("load", console.log(JSON.stringify(movieTitles)))
+}); 
